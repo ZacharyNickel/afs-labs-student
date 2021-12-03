45a090fdeffd3362a48f7090abfaa9e84531640f
ZacharyNickel <zac.m.nickel@gmail.com>
2021-12-03 17:44:39 +0000

    comment

--- database/database.sql
+++ database/database.sql
@@ -6,7 +6,7 @@
 -- NEW COMMENT - testing db pipeline pt 5
 -- NEW COMMENT THE SEQUEL
 -- new comment the third
--- new 4
+-- new 5
 
 -- Dumped from database version 9.5.4
 -- Dumped by pg_dump version 9.5.4
